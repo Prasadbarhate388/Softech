6a4c88987332ed7622b28ed998eef38cc1f9a8fc
Prasadbarhate388 <148797154+Prasadbarhate388@users.noreply.github.com>
2023-11-26 10:55:45 +0530

    Add files via upload

---
+++ find_length_of_string.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    char s1[100];
+
+    int k;
+    printf("enter the string") ;
+    scanf("%s",&s1);
+    k=strlen(s1) ;
+    printf("length of strinng is  %d",k);
+
+
+}
